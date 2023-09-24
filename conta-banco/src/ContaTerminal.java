53aa35803c7a3f83c0096d6f1c0dfd862efa028f
Simone <simonecabral.br@gmail.com>
2023-09-24 10:16:02 -0300

    Update ContaTerminal.java
    Add da lógica para possibilitar o funcionamento da aplicação.

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,7 +1,23 @@
 
 public class ContaTerminal {
-	public static void main(String[] args) throws Exception{
-		System.out.println("Hello World!");
+
+	public static void main(String[] args) {
+		int numero;
+		String agencia;
+		String nomeCliente;
+		double saldo = 233.37;
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Olá! Por favor informe o número da conta: ");
+		numero = entrada.nextInt();
+		System.out.println("Por favor informe a agência bancária: ");
+		agencia = entrada.next();
+		System.out.println("Por favor informe o nome do cliente: ");
+		nomeCliente = entrada.next();
+		
+		System.out.println("Olá, " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
 	}
 
 }
